0e64a8d66d36c38d7a09a18dc4f2f541c35c6b83
Peppimax <116319141+Peppimax@users.noreply.github.com>
2024-08-31 16:27:36 +0000

    Clase tres

---
+++ Estructuradedatos3.py
@@ -0,0 +1,58 @@
+#def factorial(numero : int) -> str | int:
+    #if numero < 0:
+        #return "No se permiten numeros neagativos."
+    #elif numero == 0 or numero == 1:
+        #return 1
+    #else:
+        #resultado = 1
+        #for i in range(2, numero + 1):
+            #resultado *= i
+        #return resultado
+
+
+#numero = int(input("Introduce un número para calcular su factorial: "))
+#print(f"El factorial de {numero} es {factorial(numero)}")
+"""
+numero_uno = int(input("Ingrese un numero: "))
+numero_dos = int(input("Ingresa otro numero: "))
+
+while True:
+    opcion = int(input('''¿Que deseaa realiar? 
+                    1. Suma
+                    2. Resta
+                    3. Multiplicacion
+                    4. Division
+                    :'''))
+    if opcion == 1:
+        resultado = numero_uno + numero_dos
+        print(f"El resultado de la suma es: {resultado}" )
+    elif opcion == 2:
+        resultado = numero_uno - numero_dos
+        print(f"El resultado de la resta es: {resultado}" )
+    elif opcion == 3:
+        resultado = numero_uno * numero_dos
+        print(f"El resultado de la multiplicacion es: {resultado}" )
+    elif opcion == 4:
+        resultado = numero_uno / numero_dos
+        print(f"El resultado de la division es: {resultado}" )
+    else:
+        print("Ingrese un valor valido")
+"""
+
+"""
+celcius = int(input("Ingrese los grados celcius que desea convertir a Fahrenheit :"))
+fahrenheit = ((celcius * 9)/5)+32
+print(f"{celcius} grados celcius equivale a {fahrenheit} grados fahrenhit")
+
+"""
+
+""""
+palabra = str(input("Ingrese a palabra que desea validar si es palindrome: "))
+palindrome = palabra[::-1]
+if palabra == palindrome:
+    print(f"la palabra {palabra} si es palidrome")
+else:
+    print(f"La palabra {palabra} no es palindrome")
+
+"""
+
